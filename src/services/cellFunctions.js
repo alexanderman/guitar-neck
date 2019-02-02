eae04fea40a5d918af352c68835b99e30ec0e10e
alexanderman <alexander.shnirman@gmail.com>
2019-02-02 14:05:32 +0200

    ..

--- src/services/cellFunctions.js
+++ src/services/cellFunctions.js
@@ -1,8 +1,18 @@
 function applyHoverEffect(stringList=[], cell, isApply=true) {
-  const { fretIdx } = cell;
-  return stringList.map(s => {
+  const { fretIdx, stringIdx } = cell;
+  return stringList.map((s, idx) => {
     let string = [...s];
-    if (string[fretIdx]) {
+    
+    /** hiligh string */
+    if (stringIdx === idx) {
+      string = string.map(sCell => {
+        return !sCell ? undefined
+          : { ...sCell, style_hover: isApply ? { opacity: 1 } : undefined }
+      })
+    }
+    
+    /** hiligh fret */
+    else if (string[fretIdx]) {
       string[fretIdx] = { 
         ...string[fretIdx],
         style_hover: isApply ? { opacity: 1 } : undefined
